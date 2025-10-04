569776f5b71a9000cb6015bd17ce55dbbbc477f4
sam_pc1 <sspadillasam@gmail.com>
2025-10-03 23:00:49 -0600

    chore(release): first commit

---
+++ .umirc.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: 'Mantenimiento Predictivo',
+  },
+  routes: [
+    {
+      path: '/',
+      redirect: '/maquinaria',
+    },
+    {
+      name: 'Maquinaria',
+      path: '/maquinaria',
+      component: './Maquinaria',
+    },
+    {
+      name: 'Control Maquinaria',
+      path: '/control-maquinaria',
+      component: './ControlMaquinaria',
+    },
+    {
+      name: 'Usuarios',
+      path: '/usuarios',
+      component: './Usuarios',
+    },
+  ],
+  npmClient: 'pnpm',
+});
